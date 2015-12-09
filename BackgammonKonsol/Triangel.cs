e56b4886601f26bfddccd299aabc42709ef68174
Giffca <Tiimmy@hotmail.se>
2015-12-10 00:36:04 +0100

    triangel class

---
+++ BackgammonKonsol/Triangel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgammonKonsol
+{
+	class Triangel
+	{
+
+		enum Colors : byte
+		{
+			Black,
+			White
+		};
+
+		private int antal;
+		private Colors color;
+	}
+}
