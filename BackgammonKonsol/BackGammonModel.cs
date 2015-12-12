7b0653dbccb3dc99dc54168ba71644f5113f8021
Giffca <Tiimmy@hotmail.se>
2015-12-12 13:13:43 +0100

    selftest

--- BackgammonKonsol/BackGammonModel.cs
+++ BackgammonKonsol/BackGammonModel.cs
@@ -6,7 +6,79 @@ using System.Threading.Tasks;
 
 namespace BackgammonKonsol
 {
+
+	enum Colors
+	{
+		Black,
+		White 
+	};
+
+
+	struct Triangel  // datatyp för plats där brickor kan ligga.
+	{
+		public int antal;
+		public Colors color;
+	}
+
+
 	class BackGammonModel
 	{
+		private Random rnd = new Random();
+
+		public int[] letsRollTheDice()
+		{
+			int[] dices = new int[4];
+			dices[2] = dices[3] = 0;
+			dices[0] = rnd.Next(1, 7);
+			dices[1] = rnd.Next(1, 7);
+			if (dices[0] == dices[1]) dices[3] = dices[2] = dices[1];
+
+			if (dices[3] == 0) return (new int[2] { dices[0], dices[1] });
+			return dices;
+		}
+
+
+
+
+
+
+		public static bool SelfTest()
+		{
+			bool ok = true;
+			BackGammonModel test = new BackGammonModel();
+
+
+			// Test för Triangel Struct
+			Triangel [] test1 = new Triangel [4];
+			test1[0].antal = 3;
+			test1[0].color = Colors.Black;
+			test1[1].antal = 1;
+			test1[1].color = Colors.White;
+
+			ok = ok && test1[0].antal == 3 && test1[0].color == Colors.Black;
+			ok = ok && test1[1].antal == 1 && test1[1].color == Colors.White;
+			ok = ok && test1[2].antal == 0 && test1[2].color == Colors.Black;
+			ok = ok && test1[3].antal == 0 && test1[3].color == Colors.Black;
+
+			ok = ok && (int)Colors.Black == 0;
+			ok = ok && (int)Colors.White == 1;
+
+			System.Diagnostics.Debug.WriteLine("Triangel " + ok);
+
+			// Test för LetsRollTheDice()
+
+			for (int i = 0; i < 100 && ok; i++)
+			{
+				int[] dices = test.letsRollTheDice();
+				ok = ok && dices[0] >= 1 && dices[0] <= 6 && dices[1] >= 1 && dices[1] <= 6;
+				if (dices[0] == dices[1]) if (dices.Length != 4) ok = false;
+			}
+
+
+			System.Diagnostics.Debug.WriteLine("LetsRollTheDice " + ok);
+			return ok;
+		}
+
+
 	}
 }
--- BackgammonKonsol/Program.cs
+++ BackgammonKonsol/Program.cs
@@ -11,7 +11,7 @@ namespace BackgammonKonsol
 		static void Main(string[] args)
 		{
 			
-			Triangel [] test1 = new Triangel [2];
+			Triangel [] test1 = new Triangel [3];
 			test1[0].antal = 3;
 			test1[0].color = Colors.Black;
 			test1[1].antal = 1;
@@ -19,14 +19,15 @@ namespace BackgammonKonsol
 
 			Console.WriteLine(test1[0].antal + " " + test1[0].color);
 			Console.WriteLine(test1[1].antal + " " + test1[1].color);
+			Console.WriteLine(test1[2].antal + " " + test1[2].color + "\n\n");  // utan initiallesering.
 
-
-			dice test = new dice();
+			BackGammonModel.SelfTest();
+			BackGammonModel test = new BackGammonModel();
 			String a = "";
 			while(a == "") 
 				{
-				int [] tarn = test.letsRollTheDice();
-				foreach(int j in tarn)
+
+				foreach(int j in test.letsRollTheDice())
 					{
 					Console.Write(j + " ");
 					}
